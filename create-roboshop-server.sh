42627f23e85399c446149ece2651a61172b35140
suni <sunilkm88@gmail.com>
2023-04-21 15:47:43 +0530

    script

--- create-roboshop-server.sh
+++ create-roboshop-server.sh
@@ -20,3 +20,23 @@ exit
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
+
+
+## Main Program
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOPs-practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+if [ -z "${AMI_ID}" ]; then
+  echo "AMI_ID not found"
+  exit 1
+fi
+
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+if [ -z "${SGID}" ]; then
+  echo "Given Security Group does not exit"
+  exit 1
+fi
+
+
+for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
+  COMPONENT="${component}-${env}"
+  create_ec2
+done
