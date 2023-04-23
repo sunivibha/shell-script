2a5490796e113289e16da96befd5a65eb2daa59b
suni <sunilkm88@gmail.com>
2023-04-23 20:44:44 +0530

    script

--- create-roboshop-server-single.sh
+++ create-roboshop-server-single.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z0571214106NI8MA27DT9"
-SG_NAME="allow all"
+ZONE_ID="Z02997672I591AG282OU5"
+SG_NAME="allow-all"
 #ENV="dev"
 #############################
 
@@ -12,11 +12,12 @@ create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t3.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
+      --iam-instance-profile Name=aws_ssm_dev_role \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-exit
+
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
@@ -35,8 +36,11 @@ if [ -z "${SGID}" ]; then
   exit 1
 fi
 
+if [ -z "$1" ]; then
+  echo Input Component Name is needed
+  exit 1
+fi
 
-for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
-  COMPONENT="${env}-${component}"
-  create_ec2
-done
+component=$1
+COMPONENT="${env}-${component}"
+create_ec2
