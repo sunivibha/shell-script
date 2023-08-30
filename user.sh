ab36142922a58807f80e578c8691f0ce17813ee5
suni <sunilkm88@gmail.com>
2023-08-30 23:19:41 +0530

    user script

--- user.sh
+++ user.sh
@@ -7,8 +7,9 @@ unzip /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
-sed -i -e 's/REDIS_ENDPOINT/redis.suvi88.online/' -e 's/MONGO_ENDPOINT/mongo.suvi88.online/' systemd.service
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl restart user
-systemctl enable user
+
+#sed -i -e 's/REDIS_ENDPOINT/redis.suvi88.online/' -e 's/MONGO_ENDPOINT/mongo.suvi88.online/' systemd.service
+#mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+#systemctl daemon-reload
+#systemctl restart user
+#systemctl enable user
