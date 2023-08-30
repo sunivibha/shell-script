05394a129e495e345f17c83f42ba0ce96eeeb511
suni <sunilkm88@gmail.com>
2023-08-30 23:13:23 +0530

    user script

---
+++ user.sh
@@ -0,0 +1,14 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+url -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+sed -i -e 's/REDIS_ENDPOINT/redis.suvi88.online/' -e 's/MONGO_ENDPOINT/mongo.suvi88.online/' systemd.service
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl restart user
+systemctl enable user
