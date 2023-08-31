9b2257c7e01d5ab8bf4e1478b07bc3897461da63
suni <sunilkm88@gmail.com>
2023-08-31 20:12:39 +0530

    pt

--- user.sh
+++ user.sh
@@ -2,7 +2,6 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
 curl -s -L -o /tmp/user.zip "<https://github.com/roboshop-devops-project/user/archive/main.zip>"
-cd /home/roboshop
 rm -rf user
 unzip -o /tmp/user.zip
 mv user-main/ user
