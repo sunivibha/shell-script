7af641ba85e7551da9c9b26ab8cd66ef15de9ef3
suni <sunilkm88@gmail.com>
2023-08-31 23:01:06 +0530

    pt

--- user.sh
+++ user.sh
@@ -4,8 +4,8 @@ useradd roboshop
 curl -s -L -o /tmp/user.zip "<https://github.com/roboshop-devops-project/user/archive/main.zip>"
 cd /home/roboshop
 rm -rf user
-unzip -o /tmp/user.zip
-mv user-main/ user
+unzip -o  /tmp/user.zip
+mv user-main user
 cd /home/roboshop/user
 npm install
 
