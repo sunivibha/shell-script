a9aeb80ae3a0597b72ff16c61f2ab2039fb02763
suni <sunilkm88@gmail.com>
2023-08-31 19:32:13 +0530

    pt

--- user.sh
+++ user.sh
@@ -2,10 +2,11 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
 curl -s -L -o /tmp/user.zip "<https://github.com/roboshop-devops-project/user/archive/main.zip>"
+cd /home/roboshop
 rm -rf user
 unzip -o /tmp/user.zip
 mv user-main user
-cd user/
+cd /home/roboshop/user
 npm install
 
 #sed -i -e 's/REDIS_ENDPOINT/redis.suvi88.online/' -e 's/MONGO_ENDPOINT/mongo.suvi88.online/' systemd.service
