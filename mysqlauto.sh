f54d3330d51fdbb8d62e73c4372c49fdd6580b43
suni <sunilkm88@gmail.com>
2023-09-06 20:43:27 +0530

    pt

--- mysqlauto.sh
+++ mysqlauto.sh
@@ -2,15 +2,52 @@ if [ -z "$1"]; then
   echo input argument password is needed
   exit
 fi
+
 ROBOSHOP_MYSQL_PASSWORD=$1
 
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [$? -eq 0 ]; then
+  echo SUCESS
+else
+  echo FAILURE
+  exit
+fi
+
+echo disable mysql 8 version repo
 dnf module disable mysql -y
+if [$? -eq 0 ]; then
+  echo SUCESS
+else
+  echo FAILURE
+  exit
+fi
 
+echo install MySQL
 yum install mysql-community-server -y
+if [$? -eq 0 ]; then
+  echo SUCESS
+else
+  echo FAILURE
+  exit
+fi
 
+echo Enable MySQL Service
 systemctl enable mysqld
+if [$? -eq 0 ]; then
+  echo SUCESS
+else
+  echo FAILURE
+  exit
+fi
+
+echo restart MySQL Service
 systemctl restart mysqld
+if [$? -eq 0 ]; then
+  echo SUCESS
+else
+  echo FAILURE
+  exit
+fi
 
 echo show databases | mysql -uroot -pROBOSHOP_MYSQL_PASSWORD
 if  [ $? -ne 0 ]
@@ -19,5 +56,3 @@ then
  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
 fi
-
-ROBOSHOP_MYSQL_PASSWORD
--- mysqlfunc.sh
+++ mysqlfunc.sh
@@ -1,23 +1,46 @@
-if [ -z "$1"]; then
+if [ -z "$1" ]; then
   echo input argument password is needed
   exit
 fi
+
 ROBOSHOP_MYSQL_PASSWORD=$1
 
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo SUCESS
+  else
+    echo FAILURE
+    exit
+  fi
+}
+
+PRINT() {
+  echo -e "\e[33m$1\e[0m"
+}
+
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+STAT $?
+
+PRINT "disable mysql 8 version repo"
 dnf module disable mysql -y
+STAT $?
 
+PRINT "install MySQL"
 yum install mysql-community-server -y
+STAT $?
 
+PRINT "Enable MySQL Service"
 systemctl enable mysqld
-systemctl restart mysqld
+STAT $?
 
-echo show databases | mysql -uroot -pROBOSHOP_MYSQL_PASSWORD
+PRINT "restart MySQL Service"
+systemctl restart mysqld
+STAT $?
+
+PRINT "show databases | mysql -uroot -pROBOSHOP_MYSQL_PASSWORD"
 if  [ $? -ne 0 ]
 then
  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'ROBOSHOP_MYSQL_PASSWORD';" > /tmp/root-pass-sql
  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
 fi
-
-ROBOSHOP_MYSQL_PASSWORD
