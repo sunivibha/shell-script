850555bcb943589fb39084ee7b91f0d59018e1a0
suni <sunilkm88@gmail.com>
2023-09-06 21:30:24 +0530

    pt

--- mysqlfunc.sh
+++ mysqlfunc.sh
@@ -40,7 +40,7 @@ STAT $?
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 if  [ $? -ne 0 ]
 then
- echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'ROBOSHOP_MYSQL_PASSWORD';" > /tmp/root-pass-sql
+ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
- cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
+ cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
 fi
